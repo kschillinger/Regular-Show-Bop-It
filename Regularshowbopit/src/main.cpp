13b3fbe8d75600a8724beaf4093b2a50d8138a4f
Kevin Schillinger <kschillinger2004@gmail.com>
2026-03-08 15:36:59 -0400

    Added delaye cleaned up logic added todo

--- Regularshowbopit/src/main.cpp
+++ Regularshowbopit/src/main.cpp
@@ -4,15 +4,29 @@
 Main File for bop it game.
 Last Updated: 3/7/2026
 Todo
+Sound selection logic
+Sound output
+Screen stuff
+accelerometer read
+photosensor read
 
      
 
 
 Open questions 
 Debouncing? hardware debounce 
-
-
+screen refresh?
+need a good way to show user where the light sensor is
+    blink led?
+    modify enclosure to make it obvious?
+df player memory addressing
+when the user restarts mid game the state case will finish executing before restarting, need a way to preemmpt and force execution to restart
+    -is it possible to send a reset signal through hardware?
+    problem with that is that a second click would be needed to take the proc out of sleep mode 
+    don't think this is the way to go
+    does the user even need to be able to restart mid game?
 */
+
 #include <Arduino.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,12 +44,15 @@ extern "C"
 //Globals
 #define REQUIRED_COUNT 10 // User must mash buttons 10 times for success
 #define REQUIRED_ACC 15 //  Arbitrary placeholder User must shake with certain acceration for success
+#define GLOBAL_DEL 5000 //start at 5s
 enum state : uint8_t{mash, shake, hide,joke,prestart,win,lose};
-uint8_t currentState;
+uint8_t currentState=prestart; //start in sleep mode
 uint16_t lbuttonCount=0,rbuttonCount=0; 
 uint8_t score = 0x00;      // Initialize score to 0
 uint8_t successful = 0x01; // Initialize to successful user input dont know if this is needed
-uint8_t delayms=5000; //start at 5s
+uint8_t delayms=GLOBAL_DEL; //initilaize delay value to max delay 
+uint8_t increment = floor(GLOBAL_DEL/100); //get delay increment 5s del div=50ms
+
 //ISRs
 void leftButtonISR()
 {
@@ -47,7 +64,22 @@ void rightButtonISR()
 }
 void startButtonISR()
 {
-     sleep_disable(); //restart execution
+     
+     if(score>0) //user restarting mid game or at end of game 
+     {
+       //reset all game vars
+       score=0;
+       lbuttonCount=0;
+       rbuttonCount=0;
+       delayms=GLOBAL_DEL;
+       currentState = rand() % (4); 
+       //preempt and restart exection somehow
+     }
+     else
+     {
+          sleep_disable(); //restart execution
+    
+     }
 }
 
 /*
@@ -58,11 +90,8 @@ void startButtonISR()
 void generateSound() //may be removed, df player needs uart function can just be called in main 
 {
 
-     
 
-}
-void readInputUpdate()
-{
+     
 
 }
 void speakerOutput(const char *phrase);
@@ -73,13 +102,26 @@ void speakerOutput(const char *phrase);
 // Display
 void display_winning_screen(void);
 void display_losing_screen(void);
-
+void screenUpdate();
 // Hardware reads
 
 //accelerometer info is i2c or spi
 bool readAccelerometer(void);
 bool  readPhotoRes(void);
-void screenUpdate();
+
+//helpers
+void updateDelay()
+{
+     if(delayms-increment<=0) //should never happen
+     {
+          delayms=200;
+     }
+     else
+     {
+          delayms-=increment;
+     }
+
+}
 
 
 int main(void)
@@ -111,11 +153,17 @@ int main(void)
                     {
                          score ++;
                          screenUpdate();
-                         currentState = rand() % (4); //randomly select next action 0-3 
-                         if(score >= 99) // winning score overwrite
+                        
+                         if(score >= 99) // winning score 
                          {
                               currentState = win;
                          }
+                         else
+                         {
+                             currentState = rand() % (4); //randomly select next action 0-3 
+                             updateDelay();
+                         }
+                        
                     }
                     else
                     {
@@ -136,11 +184,16 @@ int main(void)
                     {
                          score++;
                          screenUpdate();
-                         currentState = rand() % (4); //randomly select next action 0-3 
-                         if(score >= 99) // winning score overwrite
+                         if(score >= 99) // winning score 
                          {
                               currentState = win;
                          }
+                         else
+                         {
+                             currentState = rand() % (4); //randomly select next action 0-3 
+                             updateDelay();
+                         }
+                         
                     }
                     else
                     {
@@ -154,14 +207,20 @@ int main(void)
 
                     generateSound();
                     delay(delayms);
+
                     if(true) //should be volatage thresh check 
                     {
                          score++;
                          screenUpdate();
-                         currentState = rand() % (4); //randomly select next action 0-3 
-                         if(score >= 99) // winning score overwrite
+
+                         if(score >= 99) // winning score 
                          {
                               currentState = win;
+                         }
+                         else
+                         {
+                             currentState = rand() % (4); //randomly select next action 0-3 
+                             updateDelay();
                          }
                     }
                     else
@@ -174,10 +233,27 @@ int main(void)
 
               
                case(joke):
-                    rand() % 3;//assuming we have 2 jokes
-                    //output joke 
+
+                   
+                    generateSound();
                     delay(delayms);
-               
+                    if(true) //no input im thinking they get no points for ignoring and the delay doesn't change
+                    {
+                         currentState = rand() % (4); //transition
+                    }
+                    else //cooked
+                    {
+                         if(lbuttonCount + rbuttonCount !=0)
+                         {
+                              successful = 0; //delete?
+                              currentState = lose; //transition to lose state
+                              break;
+                         }
+                         //if accelerometer
+
+                         //if light sense 
+                         
+                    }
                     
                     break;
 
@@ -189,6 +265,8 @@ int main(void)
                     }
                     break;
                case(lose):
+               
+                    generateSound();                
                     while(1)
                     {
                          display_losing_screen();
