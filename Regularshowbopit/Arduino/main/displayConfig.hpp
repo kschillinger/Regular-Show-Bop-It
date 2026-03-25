0e7a61114696599b65da5645f8d8587f8a2a8b68
Landiss04 <l.stroh04@gmail.com>
2026-03-24 22:14:03 -0400

    Reconfigured dislpay and speaker files to align with new library calls

--- Regularshowbopit/Arduino/main/displayConfig.hpp
+++ Regularshowbopit/Arduino/main/displayConfig.hpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <LiquidCrystal_PCF8574.h>
+#include <Adafruit_LiquidCrystal.h>
 
-extern LiquidCrystal_PCF8574 lcd;
+extern Adafruit_LiquidCrystal lcd;
 
 inline void displayInit()
 {
     Wire.begin();
     lcd.begin(16, 2);
-    lcd.setBacklight(255);
+    lcd.setBacklight(HIGH);
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Regular Show");
@@ -37,7 +37,6 @@ inline void displayMessage(const char *topLine, uint8_t score)
 inline void displayStartingMessage()
 {
     lcd.clear();
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("Press Start");
-    
 }
--- Regularshowbopit/src/speakerConfig.hpp
+++ Regularshowbopit/src/speakerConfig.hpp
@@ -1,14 +1,15 @@
+#include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <DFRobotDFPlayerMini.h>
 
 #define numberJokes 5
-#define numberStates 6
+#define numberStates 7
 #define folderActions 1
 #define folderJokes 2
 
-//creating enums fore the speaker outputs, note that file structure 
-//is as follows: 
 /*
+SD card structure:
+
 /MP3
     /1 -> Actions
         001.mp3
@@ -17,137 +18,142 @@
         004.mp3
         005.mp3
         006.mp3
+        007.mp3
     /2 -> Jokes
         001.mp3
         002.mp3
         003.mp3
         004.mp3
         005.mp3
-        ....mp3
 */
 
-SoftwareSerial mp3Serial(10,11); //rx, tx for ATMEGA
+SoftwareSerial mp3Serial(10, 11);  // RX, TX for ATmega328P
 DFRobotDFPlayerMini mp3;
 
 enum state : uint8_t
 {
-     mash,
-     shake,
-     hide,
-     joke,
-     prestart,
-     win,
-     lose
+    mash,
+    shake,
+    hide,
+    joke,
+    prestart,
+    win,
+    lose
 };
 
-enum soundID_Actions : uint8_t 
+enum soundID_Actions : uint8_t
 {
-    ID_MASH  = 001,
-    ID_SHAKE = 002, 
-    ID_HIDE  = 003,
-    ID_JOKE = 004,
-    ID_EGGCELLENT = 005,
-    ID_YOURFIRED = 006,
-    ID_COUNTDOWN = 007
+    ID_MASH       = 1,
+    ID_SHAKE      = 2,
+    ID_HIDE       = 3,
+    ID_JOKE       = 4,
+    ID_EGGCELLENT = 5,
+    ID_YOURFIRED  = 6,
+    ID_COUNTDOWN  = 7
 };
 
 enum soundID_Jokes : uint8_t
 {
-    ID_NULL = 000,
-    ID_JOKE1 = 001,
-    ID_JOKE2 = 002,
-    ID_JOKE3 = 003,
-    ID_JOKE4 = 004,
-    ID_JOKE5 = 005
+    ID_NULL  = 0,
+    ID_JOKE1 = 1,
+    ID_JOKE2 = 2,
+    ID_JOKE3 = 3,
+    ID_JOKE4 = 4,
+    ID_JOKE5 = 5
 };
 
 void speakerInit()
 {
     mp3Serial.begin(9600);
-    if (!mp3.begin(mp3Serial)) {  //Use softwareSerial to communicate with mp3.
-        //Serial.println(F("Unable to begin:"));
-        //Serial.println(F("1.Please recheck the connection!"));
-        //Serial.println(F("2.Please insert the SD card!"));
-        while(true);
-    }
-    //Serial.println(F("DFPlayer Mini online."));
-    
-    mp3.setTimeOut(500); //Set serial communictaion time out 500ms
 
-    mp3.volume(10);  //Set volume value (0~30).
+    if (!mp3.begin(mp3Serial)) {
+        while (true) {
+        }
+    }
+
+    mp3.setTimeOut(500);
+    mp3.volume(10);                 // 0 to 30
     mp3.EQ(DFPLAYER_EQ_NORMAL);
     mp3.outputDevice(DFPLAYER_DEVICE_SD);
+
+    randomSeed(micros());
 }
 
-int random_joke()
+uint8_t random_joke()
 {
-    return (rand() % numberJokes) + 1; //returns a random joke ID between 1 and numberJokes
+    return random(1, numberJokes + 1);   // 1 through 5
 }
 
 soundID_Jokes randomJokeID()
 {
     switch (random_joke())
     {
-        case 1:
-            return ID_JOKE1;
-            break;
-        case 2:
-            return ID_JOKE2;
-            break;
-        case 3:
-            return ID_JOKE3;
-            break;
-        case 4:
-            return ID_JOKE4;
-            break;
-        case 5:
-            return ID_JOKE5;
-            break;
-        default:
-            return ID_NULL; //should never happen
+        case 1: return ID_JOKE1;
+        case 2: return ID_JOKE2;
+        case 3: return ID_JOKE3;
+        case 4: return ID_JOKE4;
+        case 5: return ID_JOKE5;
+        default: return ID_NULL;
     }
 }
 
-void playAndWait(uint8_t folderID, uint8_t state)
+bool playAndWait(uint8_t folderID, uint8_t trackID, unsigned long timeoutMs = 10000)
 {
-    //function that handles the playing of the audio and waiting for it to finish before execution is continued, takes in the folder ID and state ID to determine which audio to play
-    mp3.playFolder(folderID, state);
-            
-    while(true)
+    mp3.playFolder(folderID, trackID);
+
+    unsigned long startTime = millis();
+
+    while (millis() - startTime < timeoutMs)
     {
-            if(mp3.available() && mp3.readType() == DFPlayerPlayFinished)
+        if (mp3.available())
+        {
+            uint8_t eventType = mp3.readType();
+
+            if (eventType == DFPlayerPlayFinished)
             {
-                break;
+                return true;
             }
+        }
     }
+
+    return false;
 }
 
-//function that handles the sound that is to be played depending on the state
-void generateSound(uint8_t state)
+void generateSound(uint8_t currentState)
 {
-    uint8_t jokeID = randomJokeID(); //get a random joke ID to be used if the state is joke
-    switch (state)
+    uint8_t jokeID = randomJokeID();
+
+    switch (currentState)
     {
-        case (mash) :
+        case mash:
             playAndWait(folderActions, ID_MASH);
             break;
-        case (shake) :
+
+        case shake:
             playAndWait(folderActions, ID_SHAKE);
             break;
-        case (hide) :
+
+        case hide:
             playAndWait(folderActions, ID_HIDE);
             break;
-        case (joke) :
+
+        case joke:
             playAndWait(folderJokes, jokeID);
             break;
-        case (win) :
+
+        case win:
             playAndWait(folderActions, ID_EGGCELLENT);
             break;
-        case (lose) :
+
+        case lose:
             playAndWait(folderActions, ID_YOURFIRED);
             break;
-        case (prestart) :
+
+        case prestart:
             playAndWait(folderActions, ID_COUNTDOWN);
+            break;
+
+        default:
             break;
     }
 }
